5e6bc4a774a49f8c1fd41fa7dae9d6d9dc44c1e8
Matthew Boyette <Dyndrilliac@gmail.com>
2014-03-04 02:04:42 -0500

    Cleaned Up Code and Fixed Encapsulation Issues
    General code refactoring. See differential for details.

--- api/util/Networking.java
+++ api/util/Networking.java
@@ -1,9 +1,9 @@
 /*
-	Title:  Networking
+	Title: Networking
 	Author: Matthew Boyette
-	Date:   1/12/2014
-
-	This class is merely a collection of useful static methods that support code recycling. Specifically, this 
+	Date: 1/12/2014
+	
+	This class is merely a collection of useful static methods that support code recycling. Specifically, this
 	class offers methods and classes which would be useful to networking applications such as clients and servers.
 */
 
@@ -19,216 +19,28 @@ import java.util.List;
 public class Networking
 {
 	/*
-		This object encapsulates the necessary networking functionality for a simple socket-based TCP/IP client application.
-	*/
-	public static abstract class SimpleClientThread extends Thread
-	{
-		// Built-in Variables & Objects
-		private   boolean           isConnected = false;
-		private   String            remoteHost  = null;
-		private   int               remotePort  = 0;
-		protected Socket            socket      = null;
-		protected ApplicationWindow window      = null;
-		
-		// Creates a new instance of a SimpleClientThread object.
-		public SimpleClientThread(final ApplicationWindow window, final String remoteHost, final int remotePort)
-		{
-			super("SimpleClientThread");
-			this.window = window;
-			this.setRemoteHost(remoteHost);
-			this.setRemotePort(remotePort);
-			this.open();
-		}
-		
-		// Closes the connection and frees up I/O resources.
-		public void close()
-		{
-			try
-			{
-				if (this.socket != null) this.socket.close();
-			}
-			catch (final Exception e)
-			{
-				Support.displayException(this.window, e, false);
-			}
-			finally
-			{
-				this.socket = null;
-				this.setConnected(false);
-			}
-		}
-		
-		// Returns the remote host.
-		public String getRemoteHost()
-		{
-			return this.remoteHost;
-		}
-		
-		// Returns the remote TCP port.
-		public int getRemotePort()
-		{
-			return this.remotePort;
-		}
-		
-		// Returns whether or not a connection is open.
-		public boolean isConnected()
-		{
-			return this.isConnected;
-		}
-		
-		// Opens the connection and allocates needed I/O resources.
-		public void open()
-		{
-			try
-			{
-				this.socket = new Socket(this.remoteHost, this.remotePort);
-				this.setConnected(true);
-			}
-			catch (final Exception e)
-			{
-				this.close();
-				Support.displayException(this.window, e, true);
-			}
-		}
-		
-		// Empty stub. This method is defined by a sub-class.
-		public abstract void run();
-		
-		// Sets whether or not a connection is open.
-		protected void setConnected(final boolean isConnected)
-		{
-			this.isConnected = isConnected;
-		}
-		
-		// Sets the remote host.
-		protected void setRemoteHost(final String remoteHost)
-		{
-			this.remoteHost = remoteHost;
-		}
-		
-		// Sets the remote TCP port.
-		protected void setRemotePort(final int remotePort)
-		{
-			this.remotePort = remotePort;
-		}
-	}
-	
-	/*
-		This object encapsulates part of the necessary networking functionality for a simple socket-based TCP/IP server application.
-		
-		Specifically, this class represents the portion of the server which listens for new connection attempts. Every time a SimpleServerThread 
-		object accepts a new client connection, it spawns a SimpleChildServerThread object to handle that client. This allows for concurrency and 
-		a more robust server.
-	*/
-	public static abstract class SimpleServerThread extends Thread
-	{
-		// Built-in Variables & Objects
-		private   List<SimpleChildServerThread> clientList      = null;
-		private   boolean                       isListening     = false;
-		private   int                           listeningPort   = 0;
-		protected ServerSocket                  listeningSocket = null;
-		protected ApplicationWindow             window          = null;
-		
-		// Creates a new instance of a SimpleServerThread object.
-		public SimpleServerThread(final ApplicationWindow window, final int listeningPort)
-		{
-			super("SimpleServerThread");
-			this.window = window;
-			this.clientList = new LinkedList<SimpleChildServerThread>();
-			this.setListeningPort(listeningPort);
-			this.open();
-		}
-		
-		// Closes the socket and stops the listening process.
-		public void close()
-		{
-			try
-			{
-				if (this.listeningSocket != null) this.listeningSocket.close();
-			}
-			catch (final Exception e)
-			{
-				Support.displayException(this.window, e, false);
-			}
-			finally
-			{
-				this.listeningSocket = null;
-				this.setListening(false);
-			}
-		}
-		
-		// Returns the list of clients currently being handled by this server.
-		public List<SimpleChildServerThread> getClientList()
-		{
-			return this.clientList;
-		}
-		
-		// Returns the TCP port which the server is listening on for new connections.
-		public int getListeningPort()
-		{
-			return this.listeningPort;
-		}
-		
-		// Returns whether or not the server is listening for new connections.
-		public boolean isListening()
-		{
-			return this.isListening;
-		}
-		
-		// Opens the socket and begins the listening process.
-		public void open()
-		{
-			try
-			{
-				this.listeningSocket = new ServerSocket(this.getListeningPort());
-				this.setListening(true);
-			}
-			catch (final Exception e)
-			{
-				this.close();
-				Support.displayException(this.window, e, true);
-			}
-		}
-		
-		// Empty stub. This method is defined by a sub-class.
-		public abstract void run();
-		
-		// Sets the TCP port which the server is listening on for new connections.
-		protected void setListeningPort(final int listeningPort)
-		{
-			this.listeningPort = listeningPort;
-		}
-		
-		// Sets whether or not the server is listening for new connections.
-		protected void setListening(final boolean isListening)
-		{
-			this.isListening = isListening;
-		}
-	}
-	
-	/*
 		This object encapsulates part of the necessary networking functionality for a simple socket-based TCP/IP server application.
 	
 		Specifically, this class represents the portion of the server which services a user's requests. Every time a SimpleServerThread object
-		accepts a new client connection, it spawns a SimpleChildServerThread object to handle that client. This allows for concurrency and a 
+		accepts a new client connection, it spawns a SimpleChildServerThread object to handle that client. This allows for concurrency and a
 		more robust server.
 	*/
 	public static abstract class SimpleChildServerThread extends Thread
 	{
 		// Built-in Variables & Objects
-		private   boolean            isConnected = false;
-		protected SimpleServerThread parent      = null;
-		protected Socket             socket      = null;
-		private   String             userID      = null;
-		protected ApplicationWindow  window      = null;
+		private boolean					connected	= false;
+		private SimpleServerThread		parent		= null;
+		private Socket					socket		= null;
+		private String					userID		= null;
+		private ApplicationWindow		window		= null;
 		
 		// Creates a new instance of a SimpleChildServerThread object.
-		public SimpleChildServerThread(final ApplicationWindow window, final SimpleServerThread parent, final Socket socket)
+		public SimpleChildServerThread(final SimpleServerThread parent, final Socket socket, final ApplicationWindow window)
 		{
 			super("SimpleChildServerThread");
-			this.window = window;
-			this.parent = parent;
-			this.socket = socket;
+			this.setParent(parent);
+			this.setSocket(socket);
+			this.setWindow(window);
 			this.open();
 		}
 		
@@ -237,30 +49,48 @@ public class Networking
 		{
 			try
 			{
-				if (this.socket != null) this.socket.close();
+				if (this.getSocket() != null)
+				{
+					this.getSocket().close();
+				}
 			}
 			catch (final Exception e)
 			{
-				Support.displayException(this.window, e, false);
+				Support.displayException(this.getWindow(), e, false);
 			}
 			finally
 			{
-				this.socket = null;
-				this.userID = null;
+				this.setSocket(null);
+				this.setUserID(null);
 				this.setConnected(false);
 			}
 		}
 		
+		public final SimpleServerThread getParent()
+		{
+			return this.parent;
+		}
+		
+		public final Socket getSocket()
+		{
+			return this.socket;
+		}
+		
 		// Returns the identifier for the user being handled by this server thread, which is a combination of the user's IP address and local TCP port.
-		public String getUserID()
+		public final String getUserID()
 		{
 			return this.userID;
 		}
 		
-		// Returns whether or not a connection is open. 
-		public boolean isConnected()
+		public final ApplicationWindow getWindow()
 		{
-			return this.isConnected;
+			return this.window;
+		}
+		
+		// Returns whether or not a connection is open.
+		public final boolean isConnected()
+		{
+			return this.connected;
 		}
 		
 		// Opens the connection and allocates needed I/O resources.
@@ -268,23 +98,163 @@ public class Networking
 		{
 			try
 			{
-				this.userID = this.socket.getRemoteSocketAddress() + ":" + this.socket.getPort();
+				this.setUserID(this.getSocket().getRemoteSocketAddress() + ":" + this.getSocket().getPort());
 				this.setConnected(true);
 			}
 			catch (final Exception e)
 			{
 				this.close();
-				Support.displayException(this.window, e, true);
+				Support.displayException(this.getWindow(), e, true);
 			}
 		}
 		
 		// Empty stub. This method is defined by a sub-class.
+		@Override
 		public abstract void run();
 		
 		// Sets whether or not a connection is open.
-		protected void setConnected(final boolean isConnected)
+		public final void setConnected(final boolean connected)
 		{
-			this.isConnected = isConnected;
+			this.connected = connected;
+		}
+		
+		public final void setParent(final SimpleServerThread parent)
+		{
+			this.parent = parent;
+		}
+		
+		public final void setSocket(final Socket socket)
+		{
+			this.socket = socket;
+		}
+		
+		public final void setUserID(final String userID)
+		{
+			this.userID = userID;
+		}
+		
+		public final void setWindow(final ApplicationWindow window)
+		{
+			this.window = window;
+		}
+	}
+	
+	/*
+		This object encapsulates the necessary networking functionality for a simple socket-based TCP/IP client application.
+	*/
+	public static abstract class SimpleClientThread extends Thread
+	{
+		// Built-in Variables & Objects
+		private boolean				connected	= false;
+		private String				remoteHost	= null;
+		private int					remotePort	= 0;
+		private Socket				socket		= null;
+		private ApplicationWindow	window		= null;
+		
+		// Creates a new instance of a SimpleClientThread object.
+		public SimpleClientThread(final String remoteHost, final int remotePort, final ApplicationWindow window)
+		{
+			super("SimpleClientThread");
+			this.setRemoteHost(remoteHost);
+			this.setRemotePort(remotePort);
+			this.setWindow(window);
+			this.open();
+		}
+		
+		// Closes the connection and frees up I/O resources.
+		public void close()
+		{
+			try
+			{
+				if (this.getSocket() != null)
+				{
+					this.getSocket().close();
+				}
+			}
+			catch (final Exception e)
+			{
+				Support.displayException(this.getWindow(), e, false);
+			}
+			finally
+			{
+				this.setSocket(null);
+				this.setConnected(false);
+			}
+		}
+		
+		// Returns the remote host.
+		public final String getRemoteHost()
+		{
+			return this.remoteHost;
+		}
+		
+		// Returns the remote TCP port.
+		public final int getRemotePort()
+		{
+			return this.remotePort;
+		}
+		
+		public final Socket getSocket()
+		{
+			return this.socket;
+		}
+
+		public final ApplicationWindow getWindow()
+		{
+			return this.window;
+		}
+		
+		// Returns whether or not a connection is open.
+		public final boolean isConnected()
+		{
+			return this.connected;
+		}
+		
+		// Opens the connection and allocates needed I/O resources.
+		public void open()
+		{
+			try
+			{
+				this.setSocket(new Socket(this.getRemoteHost(), this.getRemotePort()));
+				this.setConnected(true);
+			}
+			catch (final Exception e)
+			{
+				this.close();
+				Support.displayException(this.getWindow(), e, true);
+			}
+		}
+		
+		// Empty stub. This method is defined by a sub-class.
+		@Override
+		public abstract void run();
+		
+		// Sets whether or not a connection is open.
+		public final void setConnected(final boolean connected)
+		{
+			this.connected = connected;
+		}
+		
+		// Sets the remote host.
+		public final void setRemoteHost(final String remoteHost)
+		{
+			this.remoteHost = remoteHost;
+		}
+		
+		// Sets the remote TCP port.
+		public final void setRemotePort(final int remotePort)
+		{
+			this.remotePort = remotePort;
+		}
+		
+		public final void setSocket(final Socket socket)
+		{
+			this.socket = socket;
+		}
+		
+		public final void setWindow(final ApplicationWindow window)
+		{
+			this.window = window;
 		}
 	}
 	
@@ -294,5 +264,127 @@ public class Networking
 	public static interface SimpleProtocol
 	{
 		public String processInput(final String input);
+	}
+	
+	/*
+		This object encapsulates part of the necessary networking functionality for a simple socket-based TCP/IP server application.
+		
+		Specifically, this class represents the portion of the server which listens for new connection attempts. Every time a SimpleServerThread
+		object accepts a new client connection, it spawns a SimpleChildServerThread object to handle that client. This allows for concurrency and
+		a more robust server.
+	*/
+	public static abstract class SimpleServerThread extends Thread
+	{
+		// Built-in Variables & Objects
+		private List<SimpleChildServerThread>	clientList		= null;
+		private boolean							listening		= false;
+		private int								listeningPort	= 0;
+		private ServerSocket					listeningSocket	= null;
+		private ApplicationWindow				window			= null;
+		
+		// Creates a new instance of a SimpleServerThread object.
+		public SimpleServerThread(final int listeningPort, final ApplicationWindow window)
+		{
+			super("SimpleServerThread");
+			this.setClientList(new LinkedList<SimpleChildServerThread>());
+			this.setListeningPort(listeningPort);
+			this.setWindow(window);
+			this.open();
+		}
+		
+		// Closes the socket and stops the listening process.
+		public void close()
+		{
+			try
+			{
+				if (this.getListeningSocket() != null)
+				{
+					this.getListeningSocket().close();
+				}
+			}
+			catch (final Exception e)
+			{
+				Support.displayException(this.getWindow(), e, false);
+			}
+			finally
+			{
+				this.setListeningSocket(null);
+				this.setListening(false);
+			}
+		}
+		
+		// Returns the list of clients currently being handled by this server.
+		public final List<SimpleChildServerThread> getClientList()
+		{
+			return this.clientList;
+		}
+		
+		// Returns the TCP port which the server is listening on for new connections.
+		public final int getListeningPort()
+		{
+			return this.listeningPort;
+		}
+		
+		public final ServerSocket getListeningSocket()
+		{
+			return this.listeningSocket;
+		}
+		
+		public final ApplicationWindow getWindow()
+		{
+			return this.window;
+		}
+		
+		// Returns whether or not the server is listening for new connections.
+		public final boolean isListening()
+		{
+			return this.listening;
+		}
+		
+		// Opens the socket and begins the listening process.
+		public void open()
+		{
+			try
+			{
+				this.setListeningSocket(new ServerSocket(this.getListeningPort()));
+				this.setListening(true);
+			}
+			catch (final Exception e)
+			{
+				this.close();
+				Support.displayException(this.getWindow(), e, true);
+			}
+		}
+		
+		// Empty stub. This method is defined by a sub-class.
+		@Override
+		public abstract void run();
+		
+		public final void setClientList(final List<SimpleChildServerThread> clientList)
+		{
+			this.clientList = clientList;
+		}
+		
+		// Sets whether or not the server is listening for new connections.
+		public final void setListening(final boolean listening)
+		{
+			this.listening = listening;
+		}
+		
+		// Sets the TCP port which the server is listening on for new connections.
+		public final void setListeningPort(final int listeningPort)
+		{
+			this.listeningPort = listeningPort;
+		}
+		
+		public final void setListeningSocket(final ServerSocket listeningSocket)
+		{
+			this.listeningSocket = listeningSocket;
+		}
+		
+		public final void setWindow(final ApplicationWindow window)
+		{
+			this.window = window;
+		}
 	}
 }
