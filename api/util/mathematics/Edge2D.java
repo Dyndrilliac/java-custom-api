af448fc42ef1167cfc1973f760c43883e922ca9f
Matthew Boyette <Dyndrilliac@gmail.com>
2015-08-17 18:35:10 -0400

    Minor Refactoring of Mathematics package.

--- api/util/mathematics/Edge2D.java
+++ api/util/mathematics/Edge2D.java
@@ -2,7 +2,7 @@
  * Title: Edge2D
  * Author: Matthew Boyette
  * Date: 10/25/2013
- *
+ * 
  * Represents an edge on a simple 2D graph. Can be weighted or non-weighted.
  */
 
@@ -14,18 +14,18 @@ public class Edge2D extends Line2D.Double
 {
 	private final static long	serialVersionUID	= 1L;
 	private double				weight				= 0;
-
+	
 	public Edge2D(final Vertex2D v1, final Vertex2D v2)
 	{
 		super(v1, v2);
 		this.weight = this.getP1().distance(this.getP2());
 	}
-
+	
 	public double getWeight()
 	{
 		return this.weight;
 	}
-
+	
 	@Override
 	public String toString()
 	{
--- api/util/mathematics/KnapsackSolver.java
+++ api/util/mathematics/KnapsackSolver.java
@@ -1,63 +1,56 @@
 /*
- * Title: 0-1 Knapsack Subset-Sum Solver
+ * Title: KnapsackSolver
  * Author: Matthew Boyette
  * Date: 5/27/2013
+ * 
+ * This object solves 0-1 Knapsack variant of the Subset-Sum problem.
  */
 
 package api.util.mathematics;
 
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.Stack;
 
 import javax.swing.JOptionPane;
 
+import api.util.Support;
+
 public class KnapsackSolver
 {
-	protected static String formatSolution(final int[] solution)
-	{
-		StringBuilder sb = new StringBuilder();
-
-		for (int element: solution)
-		{
-			sb.append(element + " ");
-		}
-
-		return sb.toString().trim();
-	}
-
 	/*
-	 * This method takes an array of weights and a knapsack capacity.
-	 * It attempts to fill the knapsack exactly with the given weights.
-	 * It only considers items in the array that are in slots start, start+1, start+2, and so on.
+	 * This method takes a list of items and a knapsack capacity.
+	 * It attempts to fill the knapsack exactly with the given items.
+	 * It only considers items in the list that are in slots start, start+1, start+2, and so on.
 	 * When the method is initially called it should be called with start equal to 0.
 	 * In addition the method also takes as input the size of the current partial solution.
 	 * This is initially 0.
 	 */
-	protected static int[] knapsack(final int[] weights, final int capacity, final int start, final int solutionSize)
+	protected static LinkedList<KnapsackItem> knapsack(final LinkedList<KnapsackItem> items, final int capacity,
+		final int start)
 	{
 		if (capacity > 0)
 		{
 			// For each i, try placing the ith item in the knapsack.
-			for (int i = start; i < weights.length; i++)
+			for (int i = start; i < items.size(); i++)
 			{
 				// Fit the smaller knapsack with items chosen from i+1, i+2, i+3...
-				int[] answer = KnapsackSolver.knapsack(weights, capacity - weights[i], i + 1, solutionSize + 1);
-
+				LinkedList<KnapsackItem> answer = KnapsackSolver.knapsack(items, capacity - items.get(i).getWeight(), i + 1);
+				
 				if (answer != null)
 				{
-					answer[solutionSize] = weights[i];
+					answer.add(items.get(i));
+					Collections.sort(items, new KnapsackItem.KnapsackItemDescending());
 					return answer;
 				}
 			}
-
+			
 			return null;
 		}
-
 		// We have found a solution. Create an array of the correct size and send it back for filling.
 		else
 			if (capacity == 0)
 			{
-				int[] temp = new int[solutionSize];
+				LinkedList<KnapsackItem> temp = new LinkedList<KnapsackItem>();
 				return temp;
 			}
 			else
@@ -66,148 +59,150 @@ public class KnapsackSolver
 				return null;
 			}
 	}
-
-	protected static String reverseSolution(final String s)
+	
+	private int										capacity		= 0;
+	private LinkedList<KnapsackItem>				items			= null;
+	private LinkedList<LinkedList<KnapsackItem>>	solutionSets	= null;
+	
+	public KnapsackSolver(final int capacity, final int[] itemWeights)
 	{
-		String retVal = "";
-		Stack<String> myStack = new Stack<String>();
-		String[] solution = s.trim().split(" ");
-
-		for (String element: solution)
+		LinkedList<KnapsackItem> items = new LinkedList<KnapsackItem>();
+		KnapsackItem item = null;
+		
+		for (int itemWeight: itemWeights)
 		{
-			myStack.push(element);
+			item = new KnapsackItem(1, itemWeight);
+			items.add(item);
 		}
-
-		while (myStack.empty() == false)
-		{
-			retVal = retVal + myStack.pop() + " ";
-		}
-
-		return retVal.trim();
+		
+		this.reset(capacity, items);
 	}
-
-	private int					capacity	= 0;
-	private LinkedList<String>	solutionSet	= null;
-	private int[]				weights		= {0};
-
-	public KnapsackSolver(final int[] weights, final int capacity)
+	
+	public KnapsackSolver(final int capacity, final LinkedList<KnapsackItem> items)
 	{
-		this.setSolutionSet(new LinkedList<String>());
-		this.setWeights(weights);
-		this.setCapacity(capacity);
-		this.populateSolutionSet();
+		this.reset(capacity, items);
 	}
-
-	protected void addSolution(final int[] solution)
+	
+	public void displaySolutionSets()
 	{
-		String formattedSolution = KnapsackSolver.formatSolution(solution);
-
-		if (!this.getSolutionSet().contains(KnapsackSolver.reverseSolution(formattedSolution)))
-		{
-			this.getSolutionSet().add(KnapsackSolver.formatSolution(solution));
-		}
-	}
-
-	public void displaySolutionSet()
-	{
-		if (this.getSolutionSet().isEmpty())
+		if (this.getSolutionSets().isEmpty())
 		{
 			JOptionPane.showMessageDialog(null,
-				"This application was unable to find any solutions for the given parameters.",
-				"No Solutions",
+				"This application was unable to find any solution sets for the given parameters.",
+				"No Solution Sets",
 				JOptionPane.WARNING_MESSAGE);
 		}
 		else
 		{
+			final int numSolutionSets = this.getSolutionSets().size();
+			final StringBuilder sb = new StringBuilder();
+			sb.append("Solution Sets Found:\n\n");
+			
+			for (int i = 0; i < numSolutionSets; i++)
+			{
+				sb.append("Solution Set [" + i + "]: " + this.getSolutionSets().get(i).toString() + "\n");
+			}
+			
 			JOptionPane.showMessageDialog(null,
-				"Optimal Solution: " +
-					KnapsackSolver.formatSolution(KnapsackSolver.knapsack(this.getWeights(), this.getCapacity(), 0, 0)) + "\n\n" +
-					"Solutions Found:\n\n" + this.getSolutionSet().toString(),
-					this.getSolutionSet().size() + " Solutions Found",
-					JOptionPane.INFORMATION_MESSAGE);
+				sb.toString(),
+				numSolutionSets + " Solutions Found",
+				JOptionPane.INFORMATION_MESSAGE);
 		}
 	}
-
+	
 	public final int getCapacity()
 	{
 		return this.capacity;
 	}
-
-	public final LinkedList<String> getSolutionSet()
+	
+	public final LinkedList<KnapsackItem> getItems()
 	{
-		return this.solutionSet;
+		return this.items;
 	}
-
-	public final int[] getWeights()
+	
+	public final LinkedList<LinkedList<KnapsackItem>> getSolutionSets()
 	{
-		return this.weights;
+		return this.solutionSets;
 	}
-
-	protected void populateSolutionSet()
+	
+	protected void populateSolutionSets()
 	{
+		final LinkedList<KnapsackItem> reverseItems = new LinkedList<KnapsackItem>();
+		LinkedList<KnapsackItem> result;
 		int counter = 0;
-		int[] reverseWeights = new int[this.getWeights().length];
-		int[] result = null;
-
-		for (int i = 0, j = (this.getWeights().length - 1); i < this.getWeights().length; i++, j--)
-		{
-			reverseWeights[j] = this.getWeights()[i];
-		}
-
+		reverseItems.addAll(this.getItems());
+		Collections.sort(reverseItems, new KnapsackItem.KnapsackItemAscending());
+		Support.displayDebugMessage(null, "Items: " + this.getItems() + "\nReversed: " + reverseItems.toString() + "\n");
+		
 		/*
 		 * TODO: Needs revision. Does not successfully find all solutions.
 		 * Given the input 32 17 15 12 10 5 3 2
 		 * Capacity = 32
 		 * Weights = {17 15 12 10 5 3 2}
-		 *
+		 * 
 		 * Finds these solutions:
-		 *
+		 * 
 		 * 17 15 = 32
 		 * 17 12 3 = 32
 		 * 17 10 5 = 32
 		 * 15 12 5 = 32
 		 * 12 10 5 3 2 = 32 (optimal solution)
-		 *
+		 * 
 		 * Misses these solutions:
-		 *
+		 * 
 		 * 17 10 3 2 = 32
 		 * 15 12 3 2 = 32
 		 * 15 10 5 2 = 32
 		 */
-
+		
 		do
 		{
-			result = KnapsackSolver.knapsack(this.getWeights(), this.getCapacity(), counter, 0);
-
+			result = KnapsackSolver.knapsack(this.getItems(), this.getCapacity(), counter);
+			
 			if (result != null)
 			{
-				this.addSolution(result);
+				if (this.getSolutionSets().contains(result) == false)
+				{
+					this.getSolutionSets().add(result);
+				}
 			}
-
-			result = KnapsackSolver.knapsack(reverseWeights, this.getCapacity(), counter, 0);
-
+			
+			result = KnapsackSolver.knapsack(reverseItems, this.getCapacity(), counter);
+			
 			if (result != null)
 			{
-				this.addSolution(result);
+				if (this.getSolutionSets().contains(result) == false)
+				{
+					this.getSolutionSets().add(result);
+				}
 			}
-
+			
 			counter++;
 		}
 		while (result != null);
 	}
-
+	
+	public void reset(final int capacity, final LinkedList<KnapsackItem> items)
+	{
+		Collections.sort(items, new KnapsackItem.KnapsackItemDescending());
+		this.setCapacity(capacity);
+		this.setItems(items);
+		this.setSolutionSets(new LinkedList<LinkedList<KnapsackItem>>());
+		this.populateSolutionSets();
+	}
+	
 	protected final void setCapacity(final int capacity)
 	{
 		this.capacity = capacity;
 	}
-
-	protected final void setSolutionSet(final LinkedList<String> solutionSet)
+	
+	protected final void setItems(final LinkedList<KnapsackItem> items)
 	{
-		this.solutionSet = solutionSet;
+		this.items = items;
 	}
-
-	protected final void setWeights(final int[] weights)
+	
+	protected final void setSolutionSets(final LinkedList<LinkedList<KnapsackItem>> solutionSets)
 	{
-		this.weights = weights;
+		this.solutionSets = solutionSets;
 	}
 }
