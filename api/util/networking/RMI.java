142f32eadf257c354a1630c9d403f6c4e379acd5
Matthew Boyette <Dyndrilliac@gmail.com>
2015-08-17 23:28:43 -0400

    Added Empty RMI Placeholder To Networking Package

--- api/util/networking/RMI.java
+++ api/util/networking/RMI.java
@@ -2,7 +2,7 @@
  * Title: RMI
  * Author: Matthew Boyette
  * Date: 4/07/2014
- *
+ * 
  * This class is merely a collection of useful static methods that support code recycling. Specifically, this
  * class offers methods and classes which would be useful to networking applications that are implemented using
  * RMI.
@@ -12,5 +12,5 @@ package api.util.networking;
 
 public final class RMI
 {
-	// TODO
+	// TODO: Add Code
 }
